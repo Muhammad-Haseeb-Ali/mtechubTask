670d343eedc699ae823aaf8eff0ec82107453116
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2023-11-21 22:12:09 +0500

    add socket in backend

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http');
+const SocketHandler = require('./socket');
 
 
 var app = express();
+var socketServer = http.createServer(app)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,5 +17,6 @@ app.use(cookieParser());
 // Serve static assets
 app.use("/public",express.static(path.join(__dirname, 'public')));
 
+SocketHandler(socketServer)
 
 module.exports = app;
---
+++ backend/socket.js
@@ -0,0 +1,16 @@
+var socketio = require('socket.io');
+var cl = require('./utils/consolesUtils')
+
+function SocketHandler (socketServer){
+    const io = socketio(socketServer)
+
+    io.on('connection', (socket) => {
+        cl.d(`New connection: ${socket.id}`);
+    
+        socket.on('message', (data) => {
+            cl.d(`New message from ${socket.id}: ${data}`);
+        })
+    })
+}
+
+module.exports = SocketHandler
