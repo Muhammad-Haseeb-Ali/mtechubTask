7ee4f43a3607b7a3506c592d56d6f8e1e231dcad
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2023-11-21 21:54:12 +0500

    add static assets route

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static assets
+app.use("/public",express.static(path.join(__dirname, 'public')));
 
 
 module.exports = app;
